b2c1ed869341e4959b3bb5b50b644ca36223473e
Scottjdgr8 <scootj31@gmail.com>
2022-10-19 21:36:08 -0700

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,20 +2,21 @@
 
 /**
  *  * print_alphabet_x10 - func
+ *  *
  *  * Return - 0
- **/
+ */
 void print_alphabet_x10(void)
 {
 	int b = 0;
-	while (b < 10)
+		while (b < 10)
 	{
 		char a = 'a';
 		while (a <= 'z')
 		{
-		_putchar(a);
+			_putchar(a);
 			a++;
 		}
-			_putchar('\n');
-			b++;
+		_putchar('\n');
+		b++;
 	}
 }
