c55874bab1621eacd62f6cbbe7e05d1b7fcb71a8
Scottjdgr8 <scootj31@gmail.com>
2022-10-25 21:59:06 -0700

    task 7

--- 7-puts_half.c
+++ 7-puts_half.c
@@ -10,10 +10,8 @@ void puts_half(char *str)
 	int index = 0, len = 0, n;
 
 	while (str[index++])
-
 		len++;
-
-	if ((len % 2) == 0)
+	if	((len % 2) == 0)
 		n = len / 2;
 	else
 		n = (len + 1) / 2;
