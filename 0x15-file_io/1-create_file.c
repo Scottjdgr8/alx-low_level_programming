3163fac938a1f231eff5e6a4aa0578ab3083c821
scottjdgr8 <scootj31@gmil.com>
2022-12-05 20:37:58 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- ** create_file - creates a file
- ** @filename: filename.
- ** @text_content: content writed in the file.
- ** Return: 1 if it success. -1 if it fails.
+ * * create_file - creates a file
+ * * @filename: filename.
+ * * @text_content: content writed in the file.
+ * *
+ * * Return: 1 if it success. -1 if it fails.
  **/
 int create_file(const char *filename, char *text_content)
 {
@@ -21,10 +22,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (!text_content)
-				text_content = "";
+		text_content = "";
 
 	for (nletters = 0; text_content[nletters]; nletters++)
-			;
+		;
 
 	rwr = write(fd, text_content, nletters);
 
